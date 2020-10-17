93d4c6052e1bf8c6d48e93b6c86481df30b23276
Alexander Tikhonov <alex.vl.tihon@gmail.com>
2020-10-17 15:08:53 +0700

    Added VirtualHostAdress structure

--- webserver.go
+++ webserver.go
@@ -6,6 +6,14 @@ type VirtualHost struct {
 	ServerName,
 	DocRoot,
 	WebServer string
-	Aliases []string
-	Ssl     bool
+	Aliases   []string
+	Ssl       bool
+	Addresses []VirtualHostAddress
+}
+
+// VirtualHostAddress repesents webserver vhost address
+type VirtualHostAddress struct {
+	IsIpv6 bool
+	Host,
+	Port string
 }
